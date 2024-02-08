1aac74658e2fdf0cb1b5b169e94efb39a19673a6
Enes <enes.saglam.bts@gmail.com>
2024-02-08 11:53:04 +0100

    siuuuu

--- commands/utility/goat.js
+++ commands/utility/goat.js
@@ -1,10 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('goat')
-		.setDescription('Replies with SIIIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU!'),
-	async execute(interaction) {
-		await interaction.reply('SIIIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU!');
-	},
+    data: new SlashCommandBuilder()
+        .setName('goat')
+        .setDescription('Replies with SIIIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU!'),
+    async execute(interaction) {
+        await interaction.reply({
+            content: 'SIIIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU!',
+            files: ['https://c.tenor.com/WfB2PNMXY6AAAAAC/tenor.gif']
+        });
+    },
 };

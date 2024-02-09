024b8cb208d71d7a0f63c465fd99d1e2ef7c6322
Moussa <mezhoudimoussa42@gmail.com>
2024-02-09 15:40:43 +0100

    zz

--- commands/utility/jeu.js
+++ commands/utility/jeu.js
@@ -3,15 +3,15 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('feet')
+        .setName('imagerandom')
         .setDescription('Affiche une image aléatoire de pied.'),
     async execute(interaction) {
         try {
-            // Appel à l'API Unsplash pour récupérer une image aléatoire de pied
+            // Appel à l'API Unsplash pour récupérer une image aléatoire de Brawl Stars
             const response = await axios.get('https://api.unsplash.com/photos/random', {
                 params: {
-                    query: 'feet', // Requête spécifique pour rechercher des images de pied
-                    client_id: 'YOUR_UNSPLASH_ACCESS_KEY' // Remplace par ta propre clé d'accès Unsplash
+                    query: 'feet',
+                    client_id: 'W8Uj-9Zbc6sx4pdHnHUmhaSxVG1gZTVho66I1GxvkXM' // Remplace par ta propre clé d'accès Unsplash
                 }
             });
 
@@ -21,8 +21,8 @@ module.exports = {
             // Envoi de l'image dans le canal où la commande a été utilisée
             await interaction.reply(imageUrl);
         } catch (error) {
-            console.error('Error fetching feet image:', error);
-            await interaction.reply('Désolé, une erreur s\'est produite lors de la récupération de l\'image de pied.');
+            console.error('Error fetching feey image:', error);
+            await interaction.reply('Désolé, une erreur s\'est produite lors de la récupération de l\'image de Brawl Stars.');
         }
     },
 };
